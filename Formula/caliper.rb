43b2d6c932f0ac241c089ef4379e97b421ed6e08
Faizan Ashiq <faizanchaodhry@gmail.com>
2026-09-17 11:04:46 +0500

    Point the Caliper formula at the v0.1.2 tarball

--- Formula/caliper.rb
+++ Formula/caliper.rb
@@ -1,8 +1,8 @@
 class Caliper < Formula
   desc "Menu bar tool for measuring distances on screen"
   homepage "https://github.com/FaizanAshiq/caliper"
-  url "https://github.com/FaizanAshiq/caliper/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "06681cc3a48a23831e2372f134f87c9c441b7923a5f9b7b2bcc8e94fcd940dd7"
+  url "https://github.com/FaizanAshiq/caliper/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "bf52593c1ff4a25b38653648cc6b1a59b315459f0b8601810e3d05fef857c066"
   license "MIT"
   depends_on macos: :sonoma
 
